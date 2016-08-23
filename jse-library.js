fcfc27060f23361fc61614c7dd9812eb68085fe6
GerritLuimstra <gerrit.luimstra@ziggo.nl>
2016-08-23 20:07:43 +0200

    Built a simple cache function
    Declared the main variables and built a simple cache function to save
    bandwidth.

--- jse-library.js
+++ jse-library.js
@@ -41,4 +41,42 @@ function JSE(JSETag, initFunction, doneFunction){
   else if($.isFunction(initFunction)){
     initFunction();
   }
+
+  /*
+    Now the validation is passed we want to acquire the required variables.
+  */
+  var JSETag = JSETag.toString();
+
+  var JSEPHPDest = JSEConfig.mainConfig.JSEPHPDest;
+  var JSEPlaceholder = JSEConfig.mainConfig.JSEPlaceholder;
+  var JSECaseSensitive = JSEConfig.mainConfig.JSECaseSensitive;
+  var JSEMeasureSpeed = JSEConfig.mainConfig.JSEMeasureSpeed;
+
+  var JSECacheEnabled = JSEConfig.cachingConfig.JSECache;
+  var JSECacheMode = JSEConfig.cachingConfig.JSECacheMode;
+  var JSECacheInterval = JSEConfig.cachingConfig.JSECacheInterval;
+
+  // If there is cache available, use it. If not, create a new empty object.
+  JSEData = window.JSEData = (typeof window.JSEData === "undefined") ? {} : window.JSEData;
+
+  // Check if the cache is not expired yet. If it is, remove the cache.
+  if(JSECacheEnabled == true){
+    if(typeof JSEData["cacheTime"] !== "undefined"){
+      if(output > (JSEData["cacheTime"] + JSECacheInterval)){
+        JSEData = {};
+      }
+    } else {
+      var d = new Date(); var hour = d.getHours(); var minute = d.getMinutes(); var second = d.getSeconds();
+      var output = parseInt((((''+hour).length<2 ? '0' :'') + hour * 3600)) + parseInt((((''+minute).length<2 ? '0' :'') + minute * 60 )) + parseInt((((''+second).length<2 ? '0' :'') + second * 1));
+      JSEData["cacheTime"] = output;
+    }
+  }
+
+  // Determine if cache should be used or not
+  var count = $.map(JSEData, function(n, i) { return i; }).length;
+  if(count == 1){
+    // rebuild the cache
+  } else {
+    // use the cache
+  }
 }
